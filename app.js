a63ca5793c11e30147ca79feccf6a3fd9827a29f
Sean Lewis <slewis100@gmail.com>
2019-09-24 10:22:17 +0100

    completed fixed nav bar

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
 
-
   burger.addEventListener('click', () => {
     //Toggle Nav
     nav.classList.toggle('nav-active');
@@ -18,11 +17,25 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
-
     //Burger animation
     burger.classList.toggle('toggle');
   });
-}
+
+  //Close munu on click
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach((link) => {
+    link.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.toggle('toggle');
+    navLinks.forEach((item) => {
+    item.style.animation = ""
+    });
+    });
+  });
+};
+
+
 
   // Accordion
 
